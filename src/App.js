f2c84659dd76f323e71274f29570f19ca7f66675
Slaveworx <tgalvao@sapo.pt>
2022-08-25 22:43:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [enteredCode, setEnteredCode] = useState("");
+  const [outputCode, setOutputCode] = useState("");
+
+  const convertHandler = () => {
+    setOutputCode(
+      enteredCode
+        .replace(/class/gi, "className")
+        .replace(/for=/gi, "forHtml=")
+        .replace(/<!--/g, "{* ")
+        .replace(/-->/g, " *}")
+        .replace(/(<img\s.*?)>/g, '$1 />')
+    );
+  };
+
+  const enteredCodeHandler = (event) => {
+    setEnteredCode(event.target.value);
+  };
+
+  const placeholder = "Insert your code here...";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>HTML TO JSX CONVERTER</h2>
+      <div className="enteredCode">
+        <textarea
+          rows="10"
+          cols="50"
+          defaultValue={placeholder}
+          onChange={enteredCodeHandler}
+        ></textarea>
+      </div>
+      <div className="convertButton">
+        <button onClick={convertHandler}> « Convert »</button>
+      </div>
+      <div className="convertedCode">
+        <textarea rows="10" cols="50" value={outputCode}></textarea>
+      </div>
     </div>
   );
 }
